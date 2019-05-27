0a4c9ae88101a9f1b59604ac97111eb70b1b83d1
Moeko Takagi <moeko-takagi@navitime.co.jp>
2019-05-27 15:33:45 +0900

    [update] Pass by reference to pointer.

--- 1-3.cpp
+++ 1-3.cpp
@@ -2,17 +2,21 @@
 #include <algorithm>
 #include <iostream>
 
-bool IsAnagram(std::string& s1, std::string& s2) {
-	if (s1.size() != s2.size()) {
+// constじゃない参照渡しは良くない
+// 可変なものはポインタで受け取る
+// 呼び出し元で可変なものだとわかる
+// 右辺値
+bool IsAnagram(std::string* s1, std::string* s2) {
+	if (s1->size() != s2->size()) {
 		return false;
 	}
-	std::sort(s1.begin(), s1.end());
-	std::sort(s2.begin(), s2.end());
-	return s1 == s2;
+	std::sort(s1->begin(), s1->end());
+	std::sort(s2->begin(), s2->end());
+	return *s1 == *s2;
 }
 
 int main() {
 	std::string a = "abc";
-	std::string b = "bcaa";
-	std::cout << IsAnagram(a, b) << std::endl;
+	std::string b = "bca";
+	std::cout << IsAnagram(&a, &b) << std::endl;
 }
